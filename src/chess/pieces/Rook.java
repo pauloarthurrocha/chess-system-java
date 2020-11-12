8a339024a7738c1b07bb96d1b90d382ad4b6aa3e
Paulo Arthur Rocha <paulo.arthu@hotmail.com>
2020-11-12 15:50:02 -0300

    Possible moves of a piece

--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -4,11 +4,11 @@ import boardgame.Board;
 import chess.ChessPiece;
 import chess.Color;
 
-public class Rook extends ChessPiece{
+public class Rook extends ChessPiece {
 
 	public Rook(Board board, Color color) {
 		super(board, color);
-	
+
 	}
 
 	@Override
@@ -16,7 +16,10 @@ public class Rook extends ChessPiece{
 		return "R";
 	}
 	
-	
-	
+	@Override
+	public boolean[][] possibleMoves() {
+		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		return mat;
+	}
 
 }
